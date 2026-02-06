81ad551ebb69aee9bc8805729c2e0948d2f90051
pinzap_ <pinzajud@gmail.com>
2026-02-06 08:18:08 +0800

    updates the layout

--- UserLogin.java
+++ UserLogin.java
@@ -1,6 +1,11 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -8,17 +13,27 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.AbstractBorder;
 
 class UserLogin {
     public static void main(String[] args) {
 
         //centered panel for login 
         JPanel loginPanel = new JPanel();
-        loginPanel.setBackground(Color.LIGHT_GRAY);
+        loginPanel.setOpaque(false);
         loginPanel.setPreferredSize(new Dimension(500, 700));
         loginPanel.setMaximumSize(new Dimension(500, 700));
         loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.Y_AXIS));
+        loginPanel.setBorder(new RoundBorderPanel(50));
 
+        //para maging round ung bg panel
+        RoundBackgroundPanel backgroundPanel = new RoundBackgroundPanel(50);
+        backgroundPanel.setBackground(Color.LIGHT_GRAY);
+        backgroundPanel.setPreferredSize(new Dimension(500, 700));
+        backgroundPanel.setMaximumSize(new Dimension(500, 700));
+        backgroundPanel.setLayout(new BoxLayout(backgroundPanel, BoxLayout.Y_AXIS));
+        backgroundPanel.add(loginPanel);
+        
         // create and customize JLabels for title
         JLabel loginTitleLabel = new JLabel();
         loginTitleLabel.setText("AOMA-Heritage Monitor"); 
@@ -84,18 +99,16 @@ class UserLogin {
         loginPanel.add(Box.createVerticalStrut(30));
         loginPanel.add(loginButton);
 
-
         //centering the login panel
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
-        centerPanel.add(loginPanel);
+        centerPanel.add(backgroundPanel);
 
         //instantiate JFrame for user login
         JFrame loginFrame = new JFrame();
         loginFrame.setTitle("AOMA-Heritage Monitor - Login Account");
         loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         loginFrame.setSize(1220, 1080);
-
 
         //shows the outputs on the frame
         loginFrame.setLayout(new BoxLayout(loginFrame.getContentPane(), BoxLayout.Y_AXIS));
@@ -107,4 +120,43 @@ class UserLogin {
         loginFrame.setVisible(true); //this will get the frame to show up
 
     }
+}
+
+class RoundBackgroundPanel extends JPanel {
+
+    private int radius;
+
+    RoundBackgroundPanel(int radius) {
+        this.radius = radius;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                            RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+
+        g2.dispose();
+        super.paintComponent(g);
+    }
+}
+
+class RoundBorderPanel extends AbstractBorder{
+    private int radius;
+
+    RoundBorderPanel(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.BLACK);
+        g2.setStroke(new BasicStroke(3));
+        g2.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+}
 }
