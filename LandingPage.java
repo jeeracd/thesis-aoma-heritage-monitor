cbdb50db8a32653f8458b9174e6dae7ec37a75d4
pinzap_ <pinzajud@gmail.com>
2026-02-10 11:52:10 +0800

    centered the interface and implements the esp32 connection via import

--- LandingPage.java
+++ LandingPage.java
@@ -1,51 +1,59 @@
-
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+import com.fazecast.jSerialComm.SerialPort; //this is used for connecting the ESP32 
 
-public class LandingPage extends JFrame implements ActionListener{
+
+public class LandingPage extends JFrame implements ActionListener {
 
     JButton landingButton;
 
-    LandingPage(){
-
-        ImageIcon landingIcon = new ImageIcon("esp-wroom-32.jpg"); //load image icon
-
-        JLabel imageLabel = new JLabel(); //create JLabel to hold the image
-        imageLabel.setIcon(landingIcon); //set the image icon to the label
-        imageLabel.setBounds(200, 250, landingIcon.getIconWidth(), landingIcon.getIconHeight()); //set position of the image
-
-        landingButton = new JButton();
-        landingButton.setBounds(200, 100, 400, 100);
-        landingButton.addActionListener(this);
-        landingButton.setText("CONNECT ESP32 HUB (via Wired Connection)");
-        landingButton.setFocusable(false);
-        landingButton.setFont(new Font("Arial", Font.BOLD,15));
-        landingButton.setOpaque(true);
-        landingButton.setForeground(Color.WHITE);
-        landingButton.setBorderPainted(false);
-        landingButton.setBackground(Color.BLUE);
-
-
+    LandingPage() {
         this.setTitle("AOMA-Heritage Monitor - Landing Page");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(1920, 1080);
+        this.setSize(1220, 1080); 
+        this.setLocationRelativeTo(null); 
+        this.setLayout(new BorderLayout());
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        centerPanel.setBackground(Color.WHITE);
+
+        ImageIcon esp32Image = new ImageIcon("esp-wroom-32.jpg");
+        Image scaledImage = esp32Image.getImage().getScaledInstance(350, 350, Image.SCALE_SMOOTH); 
+        ImageIcon resizedImage = new ImageIcon(scaledImage);
+
+        JLabel imageLabel = new JLabel(resizedImage);
+        imageLabel.setAlignmentX(Component.CENTER_ALIGNMENT); 
+
+        landingButton = new JButton("CONNECT ESP32 HUB (via Wired Connection)");
+        landingButton.setAlignmentX(Component.CENTER_ALIGNMENT); 
+        landingButton.setPreferredSize(new Dimension(400, 50));
+        landingButton.setMaximumSize(new Dimension(400, 50));
+        landingButton.setMinimumSize(new Dimension(400, 50));
+        landingButton.setFont(new Font("Arial", Font.BOLD, 15));
+        landingButton.setFocusable(false);
+        landingButton.setForeground(Color.WHITE);
+        landingButton.setBackground(Color.BLUE);
+        landingButton.setOpaque(true);
+        landingButton.setContentAreaFilled(true);
+        landingButton.setBorderPainted(false);
+        landingButton.addActionListener(this);
+
+        centerPanel.add(Box.createVerticalGlue());
+        centerPanel.add(imageLabel);
+        centerPanel.add(Box.createRigidArea(new Dimension(0, 30))); 
+        centerPanel.add(landingButton);
+        centerPanel.add(Box.createVerticalGlue());
+
+        this.add(centerPanel, BorderLayout.CENTER);
         this.setVisible(true);
-
-        this.add(landingButton); //show button to the frame
-        this.add(imageLabel);
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == landingButton) { //action to be performed when button is clicked
+        if (e.getSource() == landingButton) {
             System.out.println("Test click in landing page button");
         }
     }
