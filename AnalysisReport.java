e33b0dceac7e09b65f9240e576940830bd72ff6b
pinzap_ <pinzajud@gmail.com>
2026-02-15 14:10:14 +0800

    successfully connects with the setupconnection.java

--- AnalysisReport.java
+++ AnalysisReport.java
@@ -1,3 +1,18 @@
-public class AnalysisReport {
-    
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class AnalysisReport extends JPanel {
+
+    public AnalysisReport() {
+        setLayout(new BorderLayout());
+
+        JLabel title = new JLabel("Analysis Dashboard", JLabel.CENTER);
+        title.setFont(new Font("Arial", Font.BOLD, 28));
+
+        add(title, BorderLayout.NORTH);
+
+
+    }
 }
