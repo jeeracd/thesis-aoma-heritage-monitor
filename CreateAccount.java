310324b486088e81ebbd832001a268df56b57d3c
pinzap_ <pinzajud@gmail.com>
2026-02-22 16:05:47 +0800

    initial commit

---
+++ CreateAccount.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class CreateAccount extends JFrame {
+    public CreateAccount() {
+        setTitle("AOMA-Heritage Monitor - Create Account");
+        setSize(700, 800);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+public static void main(String[] args) {
+    new CreateAccount();
+}
+}
