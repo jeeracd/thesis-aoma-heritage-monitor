37819d8e816e01da294d51aa28311b2ea5591aad
pinzap_ <pinzajud@gmail.com>
2026-03-06 12:26:59 +0800

    nagdelete ng lumang files

--- HeadHelpPanel.java
+++
@@ -1,6 +0,0 @@
-public class HeadHelpPanel {
-    public HeadHelpPanel() {
-
-    }
-
-}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,2 +1,4 @@
-
-        JPanel headerArea = new JPanel(new BorderLayout());
+JLabel datasetExecLbl = new JLabel("DATASET ID: #20260224-OMA-005");
+        datasetExecLbl.setFont(normalFont);
+        datasetExecLbl.setBounds(750, 100, 350, 25);
+        reportPanel.add(datasetExecLbl);
